11c3739bc83efa56144754c0ed28ce14fdebe273
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2023-08-11 14:28:38 +0200

    added profiler

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,15 +1,15 @@
 // Production performance monitoring
 // http://localhost:3000/isolated/exercise/07.js
 
-import * as React from 'react'
+import * as React from 'react';
 // 🐨 you're going to need the reportProfile function
 // 💰 here, let me help you with that...
-// import reportProfile from '../report-profile'
+import reportProfile from '../report-profile';
 
 function Counter() {
-  const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>{count}</button>
+  const [count, setCount] = React.useState(0);
+  const increment = () => setCount(c => c + 1);
+  return <button onClick={increment}>{count}</button>;
 }
 
 function App() {
@@ -20,16 +20,18 @@ function App() {
       give it the ID of "counter" and pass reportProfile
       to the onRender prop.
       */}
-      <div>
-        Profiled counter
-        <Counter />
-      </div>
+      <React.Profiler id="counter" onRender={reportProfile}>
+        <div>
+          Profiled counter
+          <Counter />
+        </div>
+      </React.Profiler>
       <div>
         Unprofiled counter
         <Counter />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
